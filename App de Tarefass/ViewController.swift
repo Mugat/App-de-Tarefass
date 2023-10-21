56f9768a263c845831e0e39f5cc3cbe598e70bb5
Mugat <lucasofjus@gmail.com>
2023-10-21 16:57:28 -0400

    Initial Commit

---
+++ App de Tarefass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App de Tarefass
+//
+//  Created by user244585 on 10/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
